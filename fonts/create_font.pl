7cdea94b577d42300ffdcb2be417b11fe4e3a1b9
eroux <eroux@49dc95eb-3262-46e5-aec4-4b13905ba261>
2007-07-03 17:54:45 +0000

    adding NEWS file, and changing the license to GPLv3

--- fonts/create_font.pl
+++ fonts/create_font.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl -w
 
-#Perl script to build a complete font for omega from afm and tfm files.
-#Copyright (C) 2006 Elie Roux
+#Perl script to build a complete ovp font for omega from afm and tfm files.
+#Copyright (C) 2007 Elie Roux <elie.roux@enst-bretagne.fr>
 #
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; either version 2
-#of the License, or (at your option) any later version.
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
 #
 #This program is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,8 +14,7 @@
 #GNU General Public License for more details.
 #
 #You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 
