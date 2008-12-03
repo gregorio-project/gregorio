b83ff081346bf2a319b534641ad5bbc71691b040
eroux <eroux@49dc95eb-3262-46e5-aec4-4b13905ba261>
2008-12-03 15:05:08 +0000

    updating the license

--- src/gregorio-utils.c
+++ src/gregorio-utils.c
@@ -94,12 +94,15 @@ print_licence ()
 {
   printf ("\n\
 Tools for manipulation of gregorian chant files\n\
+Copyright (C) 2006 Elie Roux <elie.roux@telecom-bretagne.eu>\n\
+\n\
+Gregorio command line interface.\n\
 Copyright (C) 2006 Elie Roux <elie.roux@enst-bretagne.fr>\n\
 \n\
-This program is free software; you can redistribute it and/or\n\
-modify it under the terms of the GNU General Public License\n\
-as published by the Free Software Foundation; either version 2\n\
-of the License, or (at your option) any later version.\n\
+This program is free software: you can redistribute it and/or modify\n\
+it under the terms of the GNU General Public License as published by\n\
+the Free Software Foundation, either version 3 of the License, or\n\
+(at your option) any later version.\n\
 \n\
 This program is distributed in the hope that it will be useful,\n\
 but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
@@ -107,8 +110,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
 GNU General Public License for more details.\n\
 \n\
 You should have received a copy of the GNU General Public License\n\
-along with this program; if not, write to the Free Software\n\
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.\n\n");
+along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\n");
 }
 
 static void
