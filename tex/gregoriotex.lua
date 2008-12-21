474a56c49a0071306d1bc7793e70142b359d4080
eroux <eroux@49dc95eb-3262-46e5-aec4-4b13905ba261>
2008-12-21 22:03:40 +0000

    correcting the bug of additional dumb lines with LuaTeX (?)

--- tex/gregoriotex.lua
+++ tex/gregoriotex.lua
@@ -1,9 +1,20 @@
 local hlist = node.id('hlist')
+local vlist = node.id('vlist')
 tempnode=node.new(node.id('glyph'), 0)
 tempnode.font=65 --% TODO : don't know why 65...
 tempnode.char=tex.defaulthyphenchar
-dashnode=node.hpack(tempnode)
-dashnode.shift=393216 --% TODO : a less static value
+--dashnode=node.hpack(tempnode)
+--dashnode.shift=393216 --% TODO : a less static value
+
+-- the algorithm here is simple: we look at the number of hlists in a line. If it is two, it means that it is a dumb line, and we remove it...
+function removedumblines(h, groupcode, glyphes)
+    for line in node.traverse_id(hlist, h) do
+        if node.count(hlist, line.list) == 2 then
+            node.remove(h, line)
+        end
+    end
+    return true
+end
 
 function addhyphen(h, groupcode, glyphes)
     local lastseennode=nil
@@ -40,5 +51,5 @@ function addhyphen(h, groupcode, glyphes)
 end 
 
 function atScoreBeggining()
-    callback.register('post_linebreak_filter', addhyphen)
+    callback.register('post_linebreak_filter', removedumblines)
 end
