be1bfce0a11ae95c3c449ea40b764d1d7fac2716
eroux <eroux@49dc95eb-3262-46e5-aec4-4b13905ba261>
2008-12-22 12:20:59 +0000

    fixing the bug of missing hyphens in luatex

--- tex/gregoriotex.lua
+++ tex/gregoriotex.lua
@@ -1,10 +1,6 @@
 local hlist = node.id('hlist')
 local vlist = node.id('vlist')
-tempnode=node.new(node.id('glyph'), 0)
-tempnode.font=65 --% TODO : don't know why 65...
-tempnode.char=tex.defaulthyphenchar
---dashnode=node.hpack(tempnode)
---dashnode.shift=393216 --% TODO : a less static value
+local glyph = node.id('glyph')
 
 -- the algorithm here is simple: we look at the number of hlists in a line. If it is two, it means that it is a dumb line, and we remove it...
 function removedumblines(h, groupcode, glyphes)
@@ -18,38 +14,60 @@ end
 
 function addhyphen(h, groupcode, glyphes)
     local lastseennode=nil
-    local attributeid=1
-    local potentialdashvalue=3
+    local attributeid=987
+    local potentialdashvalue=1
     local nopotentialdashvalue=2
     local adddash=false
+    local tempnode=node.new(glyph, 0)
+    local dashnode
+    tempnode.font=0
+    tempnode.char=tex.defaulthyphenchar
+    dashnode=node.hpack(tempnode)
+    dashnode.shift=0
     --% we explore the lines
     for a in node.traverse_id(hlist, h) do
         for b in node.traverse_id(hlist, a.list) do
-            if adddash == false then
-                if node.has_attribute(b, attributeid, potentialdashvalue) then
-                    adddash=true
-                    lastseennode=b
-                    attr = b.attr.next
-                    --texio.write_nl('ATTR number = ' .. attr.number .. ' value = ' .. attr.value)
+            --if node.has_attribute(b, attributeid, 2) then
+            --    texio.write_nl('prout')
+            --end
+            if node.has_attribute(b, attributeid, potentialdashvalue) then
+                adddash=true
+                lastseennode=b
+                --attr = b.attr.next
+                -- texio.write_nl('ATTR number = ' .. attr.number .. ' value = ' .. attr.value)
+                -- here we set up the font number of the hyphen
+                if (tempnode.font == 0) then
+                    for g in node.traverse_id(glyph, b.list) do
+                        tempnode.font = g.font
+                        break
+                    end
                 end
-            else
-                if node.has_attribute(b, attributeid, nopotentialdashvalue) then
-                    adddash=false
-                    attr = b.attr.next
-                    --texio.write_nl('ATTR number = ' .. attr.number .. ' value = ' .. attr.value)
-                end 
+                if dashnode.shift==0 then
+                    dashnode.shift = b.shift
+                end
+            -- if we encounter a text that doesn't need a dash, we acknowledge it
+            elseif node.has_attribute(b, attributeid, nopotentialdashvalue) then
+                adddash=false
+                --attr = b.attr.next
+                -- texio.write_nl('ATTR number = ' .. attr.number .. ' value = ' .. attr.value)
             end
         end
         if adddash==true then
             local temp= node.copy(dashnode)
-            --%TODO: remove the last glue in b first, and insert a glue of ancient glue - withdof(dashnode) after the dash
-            node.insert_after(a.list, b, temp)
             addash=false
         end
+        -- we reinitialize the shift value, because it may change according to the line
+        dashnode.shift=0
     end
     return true
 end 
 
+function gregorioCallback(h, groupcode, glyphes)
+    removedumblines(h, groupcode, glyphes)
+    addhyphen(h, groupcode, glyphes)
+    return true
+end
+
 function atScoreBeggining()
-    callback.register('post_linebreak_filter', removedumblines)
+    callback.register('post_linebreak_filter', gregorioCallback)
 end
