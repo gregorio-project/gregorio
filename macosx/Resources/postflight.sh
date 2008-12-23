f3d4dabb09dc4b3261869d267252705fc1bd2b62
tllenos <tllenos@49dc95eb-3262-46e5-aec4-4b13905ba261>
2008-12-23 07:10:53 +0000

    making the information in the OS X text files more useful

--- macosx/Resources/postflight.sh
+++ macosx/Resources/postflight.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 #Script to move the Gregorio examples and such to a more 
 #convenient place (in the user's home directory) and to -globally-
-#install the fonts in the user's TeX distribution ... I don't know
-#if this is actually a `good' place yet...
+#install the fonts in the user's TeX distribution
 
 TEMPDIR="/private/tmp/gregorio"
-GREUSERDIR="$HOME/Library/Gregorio"
+GREUSERDIR="$HOME/Documents/Gregorio"
 GREFONTDIR="$GREUSERDIR/fonts"
 
 mkdir -p $GREUSERDIR
