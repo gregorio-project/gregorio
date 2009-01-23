12281af12c29571b1391c70a8ee75758d23e1241
eroux <eroux@49dc95eb-3262-46e5-aec4-4b13905ba261>
2009-01-23 11:11:53 +0000

    adding the possibility to have three ictus a

--- tex/gregoriotex-ictus.lua
+++ tex/gregoriotex-ictus.lua
@@ -107,18 +107,13 @@ function chiroPrintLine()
   if chiroList == nil or chiroCurrentScore > table.maxn(chiroList.scores) then
     return
   elseif chiroCurrentLine > table.maxn(chiroList.scores[chiroCurrentScore].lines) then
-    texio.write_nl('linea' .. chiroCurrentLine)
-    texio.write_nl('lineg' .. chiroCurrentScore)
-    texio.write_nl('scoreg' .. table.maxn(chiroList.scores[chiroCurrentScore].lines))
     -- if there are too many calls in a score
     return
   elseif table.maxn(chiroList.scores) < chiroCurrentScore  or chiroCurrentScore == 0 then
-    texio.write_nl('lineb' .. chiroCurrentLine)
     -- a basic check
     return
   else
     --printTable(chiroList.scores[chiroCurrentScore].lines[chiroCurrentLine], '')
-    texio.write_nl('linec' .. chiroCurrentLine)
     chiroPrintCLine (chiroList.scores[chiroCurrentScore].lines[chiroCurrentLine])
     chiroCurrentLine = chiroCurrentLine + 1
   end
@@ -151,37 +146,65 @@ function chiroPrintCLine (line)
   local endPos = line.begin + line.width
   local currentPos = startPos
   local previousLen = 0
+  local nextLen = 0
   local path = nil
   local i = 1
   local firstShift = nil -- the kern we'll have to do before just printing the mplib
   local temp -- a stupid temporary value
   local first = 1 -- useful for a test (we can't test i because sometimes i == 2 the first time)
+  local lastvpos = 3 -- here the default value is 3
   -- first we deal with the last point of the line:
   -- we add a point at min (lastpoint.pos + 20, endPos), except if it's the last line (TODO: check if it's the last line)
   table.insert(line.ictus, {['type'] = it, ['pos'] = math.min(line.ictus[table.maxn(line.ictus)].pos + 20, endPos)})
-  -- then we set path to the good path section (see iait and friends for details path variable
-  -- we can't do a for in loop, because sometimes we have only one function for two or three ictus
+  -- then we set path to the good path section (see iait and friends for details path variable)
+  -- we can't do a for .. in .. loop, because sometimes we have only one function for two or three ictus
+  -- basically we draw the shape betwee line.ictus[i] and line.ictus[i+1] (or i+2 or 3)
   while line.ictus[i+1] do
     if line.ictus[i].type == it then
-      if line.ictus[i+1].type == ia then
-        path, temp = itia(line.ictus[i].pos, line.ictus[i+1].pos, previousLen, printSmallBars)
+      if line.ictus[i+2] ~= nil then
+        nextLen = line.ictus[i+2].pos  - line.ictus[i+1].pos
       else
-        path, temp = itit(line.ictus[i].pos, line.ictus[i+1].pos, previousLen, printSmallBars)
+        nextLen = 0
+      end
+      if line.ictus[i+1].type == ia then
+        path, temp, vpos = itia(line.ictus[i].pos, line.ictus[i+1].pos, previousLen, printSmallBars, lastvpos, nextLen)
+      else
+        path, temp, vpos = itit(line.ictus[i].pos, line.ictus[i+1].pos, previousLen, printSmallBars, lastvpos, nextLen)
       end
       previousLen = 0
       i = i + 1
     else
       if line.ictus[i+1].type == it then
+        if line.ictus[i+2] ~= nil then
+          nextLen = line.ictus[i+2].pos  - line.ictus[i+1].pos
+        else
+          nextLen = 0
+        end
         previousLen = (line.ictus[i+1].pos - line.ictus[i].pos)
-        path, temp = iait(line.ictus[i].pos, line.ictus[i+1].pos, 0, printSmallBars)
+        path, temp, vpos = iait(line.ictus[i].pos, line.ictus[i+1].pos, 0, printSmallBars, lastvpos, nextLen)
         i = i + 1
-      else
-        path, temp = itit(line.ictus[i].pos, line.ictus[i+1].pos, previousLen, printSmallBars)
+      elseif line.ictus[i+2] ~= nil and line.ictus[i+2].type == it then
+        if line.ictus[i+3] ~= nil then
+          nextLen = line.ictus[i+3].pos  - line.ictus[i+2].pos
+        else
+          nextLen = 0
+        end
+        path, temp, vpos = iaiait(line.ictus[i].pos, line.ictus[i+1].pos, line.ictus[i+2].pos, 0, printSmallBars, lastvpos, nextLen)
+        previousLen = 0
+        i = i + 2
+      elseif line.ictus[i+2] ~= nil and line.ictus[i+3] ~= nil and line.ictus[i+2].type == ia then
+        if line.ictus[i+3] ~= nil then
+          nextLen = line.ictus[i+3].pos  - line.ictus[i+2].pos
+        else
+          nextLen = 0
+        end
+        path, temp, vpos = iaiaiait(line.ictus[i].pos, line.ictus[i+1].pos, line.ictus[i+2].pos, line.ictus[i+3].pos, previousLen, printSmallBars, lastvpos, nextLen)
         previousLen = 0
         --path = iaiait
-        i = i + 1
+        i = i + 3
       end
     end
+    lastvpos = vpos
     -- path contains something like p:= a .. path;
     -- and also something like draw the .. small .. path .. of .. the .. bar;
     if first == 1 then
@@ -201,7 +224,7 @@ function chiroPrintCLine (line)
   end
   s = s .. "draw q;\nendfig;\n\\endmplibcode\n"
   tex.print(s)
-  texio.write_nl(s)
+  -- texio.write_nl(s)
 end
 
 -- function that returns a string like "draw mypath;" where mypath is a small 
@@ -223,7 +246,7 @@ function iaitlastangle (width)
   return (30 - 0.2 * (width))
 end
 
-function iait (ibegin, iend, first, printSmallBar)
+function iait (ibegin, iend, first, printSmallBar, beginvpos, nextLen)
   local init_height
   if first == 1 then
     init_height = 0
@@ -244,7 +267,7 @@ function iait (ibegin, iend, first, printSmallBar)
   return path, iaitfirstdiameter(ibegin, iend)/2
 end
 
-function iaiait (ibegin, imiddle, iend, first, printSmallBar)
+function iaiait (ibegin, imiddle, iend, first, printSmallBar, beginvpos, nextLen)
   local maxdiff-- a value we will use several times
   if imiddle - ibegin < iend - imiddle then
     maxdiff = (20 - (imiddle - ibegin))/2
@@ -258,27 +281,80 @@ function iaiait (ibegin, imiddle, iend, first, printSmallBar)
     init_height = 3
   end
   local path =  string.format("p := (%.01f, %.01f){left} .. {right}(%.01f, %.01f){right} .. {down}(%.01f, %.01f){down} .. {up}(%.01f, %.01f){up} .. {right}(%.01f,%.01f){right} .. {dir-%d}(%.01f,%.01f);\n",
+  -- (%.01f, %.01f){left}
   ibegin,
   init_height,
+  -- {right}(%.01f, %.01f){right}
   ibegin,
-  27 + (20 - (imiddle - ibegin))/4,
+  24 + (20 - (imiddle - ibegin))/3,
+  -- {down}(%.01f, %.01f){down}
   imiddle+3,
-  13 + maxdiff,
+  10 + maxdiff,
+  -- {up}(%.01f, %.01f){up}
   imiddle-3,
-  13 + maxdiff,
+  10 + maxdiff,
+  -- {right}(%.01f,%.01f){right}
   imiddle + (iend - imiddle)/3,
-  27 + (20 - (imiddle - ibegin))/4,
+  24 + (20 - (imiddle - ibegin))/3,
+  -- {dir-%d}(%.01f,%.01f)
   30,
   iend,
   3)
   if printSmallBar == 1 then
     path = path .. chiroPrintBar(ibegin, 3)
-    path = path .. chiroPrintBar(imiddle, 10 + maxdiff)
+    path = path .. chiroPrintBar(imiddle, 7 + maxdiff)
   end
   return path, 0
 end
 
-function itia (ibegin, iend, previouslen, printSmallBar)
+function iaiaiait (ibegin, imiddle1, imiddle2, iend, first, printSmallBar, beginvpos, nextLen)
+  local maxdiff-- a value we will use several times
+  maxdiff = math.max(imiddle1 - ibegin, imiddle2 - imiddle1, iend - imiddle2)
+  maxdiff = 20 - maxdiff/2
+  local init_height
+  if first == 1 then
+    init_height = 0
+  else
+    init_height = 3
+  end
+  local path =  string.format("p := (%.01f, %.01f){left} .. {right}(%.01f, %.01f){right} .. {down}(%.01f, %.01f){down} .. {up}(%.01f, %.01f){up} .. {right}(%.01f,%.01f){right} .. {down}(%.01f,%.01f){down} .. {up}(%.01f, %.01f){up} .. {right}(%.01f,%.01f){right} .. {dir-%d}(%.01f,%.01f);\n",
+  -- (%.01f, %.01f){left}
+  ibegin,
+  init_height,
+  -- {right}(%.01f, %.01f){right}
+  ibegin,
+  24 + (20 - (imiddle1 - ibegin))/3,
+  -- {down}(%.01f, %.01f){down}
+  imiddle1+3,
+  10 + maxdiff,
+  -- {up}(%.01f, %.01f){up}
+  imiddle1-3,
+  10 + maxdiff,
+  -- {right}(%.01f,%.01f){right}
+  imiddle1 + (imiddle2 - imiddle1)/3,
+  24 + (20 - (imiddle1 - ibegin))/3,
+  -- {down}(%.01f,%.01f){down}
+  imiddle2+3,
+  10 + maxdiff,
+  -- {up}(%.01f, %.01f){up}
+  imiddle2-3,
+  10 + maxdiff,
+  --{right}(%.01f,%.01f){right}
+  imiddle2 + (iend - imiddle2)/3,
+  24 + (20 - (imiddle2 - ibegin))/3,
+  -- {dir-%d}(%.01f,%.01f)
+  30,
+  iend,
+  3)
+  if printSmallBar == 1 then
+    path = path .. chiroPrintBar(ibegin, 3)
+    path = path .. chiroPrintBar(imiddle1, 7 + maxdiff)
+    path = path .. chiroPrintBar(imiddle2, 7 + maxdiff)
+  end
+  return path, 0
+end
+
+function itia (ibegin, iend, previouslen, printSmallBar, beginvpos, nextLen)
   local path = string.format("p := (%.01f, 3){dir-%d} .. {dir30}(%.01f, 3);\n",
   ibegin,
   iaitlastangle (previouslen),
@@ -289,7 +365,7 @@ function itia (ibegin, iend, previouslen, printSmallBar)
   return path, 0
 end
 
-function itit (ibegin, iend, previouslen, printSmallBar)
+function itit (ibegin, iend, previouslen, printSmallBar, beginvpos, nextLen)
   local path = string.format("p := (%.01f, 3){dir-%d} .. {dir-30}(%.01f, 3);\n",
   ibegin,
   iaitlastangle (previouslen),
