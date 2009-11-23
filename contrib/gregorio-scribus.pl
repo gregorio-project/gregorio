501809a90bbe11f2a3457f40a8f69ad395ac9d94
eroux <eroux@49dc95eb-3262-46e5-aec4-4b13905ba261>
2009-11-23 07:46:45 +0000

    licensing the Scribus+gregorio files under GPLv2 or later, to enable a future inclusion in Scribus

--- contrib/gregorio-scribus.pl
+++ contrib/gregorio-scribus.pl
@@ -3,9 +3,9 @@
 # gregorio-scribus, a script to use gregorio inside scribus.
 # Copyright (C) 2009 Pierre Couderc <pierre@couderc.eu>.
 # 
-# This program is free software: you can redistribute it and/or modify
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -13,14 +13,9 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# See the different comments in the file to change some options in order
-# for it to work.
-#
-# Do not forget that you must have perl installed under Windows in order
-# for it to work.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 use strict;
 
