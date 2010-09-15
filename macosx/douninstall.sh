a427b2c4dc76d3c24e2504504ef02a2952ddb9b9
tllenos <tllenos@49dc95eb-3262-46e5-aec4-4b13905ba261>
2010-09-15 04:49:33 +0000

    some Mac Package things * include font license info * update uninstaller for next version (no library issues)

--- macosx/douninstall.sh
+++ macosx/douninstall.sh
@@ -1,20 +1,17 @@
-#!/bin/sh
-#Script to uninstall the gregorio Mac OS X distribution.
+#!/bin/bash
+# Script to uninstall the gregorio Mac OS X distribution.
+# Perhaps we should also uninstall the GregorioTeX stuff...
+# But that is a problem for another day
 
 PREFIX="/usr/local"
 BINDIR="$PREFIX/bin"
-LIBDIR="$PREFIX/lib"
-GRELIBDIR="$LIBDIR/gregorio"
-GREINCLUDEDIR="$PREFIX/include/gregorio"
+#LIBDIR="$PREFIX/lib"
+#GRELIBDIR="$LIBDIR/gregorio"
+#GREINCLUDEDIR="$PREFIX/include/gregorio"
 RECEIPTDIR="/Library/Receipts"
 
 rm $BINDIR/gregorio
-rm -dR $GRELIBDIR
-rm $LIBDIR/pkgconfig/gregorio.pc
-rm $LIBDIR/libgregorio*.dylib
-rm $LIBDIR/libgregorio*.la
-rm -dR $GREINCLUDEDIR
-rm -dR $RECEIPTDIR/gregorio-1.0.pkg
+rm -dR $RECEIPTDIR/gregorio-2.0.pkg
 
 echo "Gregorio uninstalled. You may now delete the Gregorio folder\ncontaining this script and \
 the example files if you wish.\nThanks!"
