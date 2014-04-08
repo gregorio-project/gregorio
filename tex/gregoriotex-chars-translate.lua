0b320a19e2789036ec26ab8e9a9c835b10a86e29
eroux <eroux@49dc95eb-3262-46e5-aec4-4b13905ba261>
2014-04-08 10:34:49 +0000

    Adding script to change unicode range (not using it yet)

---
+++ tex/gregoriotex-chars-translate.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env texlua
+
+infile = io.open("gregoriotex-chars.tex.in");
+outfile = io.open("gregoriotex-chars.tex", 'w');
+
+charrangestart = 0
+
+for line in infile:lines() do
+  newline = line
+  if (string.sub(line,1,1)~='%') then
+  newline = string.gsub(line, '([0-9]+)', function(match)
+        if (match ~= '0' and match ~= '1') then
+          return match+charrangestart
+        end
+      end)
+    end
+  outfile:write(newline.."\n")
+end
+
+infile:close()
+outfile:close()
