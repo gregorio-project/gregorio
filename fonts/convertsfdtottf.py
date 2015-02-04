0a0ce4261a368ffada524c504ea675e865002dba
Br. Samuel Springuel <rpspringuel@gmail.com>
2015-02-04 11:25:07 -0500

    First try at python script for font conversion.
    Note: I'm having trouble getting the fontforge python extensions installed and so haven't tested this yet.  It's modeled on squarize.py though, so I think it should work.

---
+++ fonts/convertsfdtottf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# coding=utf-8
+
+from __future__ import print_function
+
+import getopt, sys
+import fontforge
+
+def usage():
+    print("""
+Python script to confert a fontforge native file (.sfd) to a TrueType font
+(.ttf).
+
+Usage:
+    convertsfdtottf fontname
+
+with fontname=gresym or greextra for now.
+""")
+
+def main():
+#    global font, font_name
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h", ["help"])
+    except getopt.GetoptError:
+        # print help information and exit:
+        usage()
+        sys.exit(2)
+    output = None
+    verbose = False
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit()
+    if len(args)==0:
+        usage()
+        sys.exit(2)
+    if args[0] == "gresym":
+        font_name="gresym"
+    elif args[0] == "greextra":
+        font_name="greextra"
+    else:
+        usage()
+        sys.exit(2)
+    font = fontforge.open("%s.sfd" % font_name)
+    font.generate("%s.ttf" % font_name)
+    font.close()
