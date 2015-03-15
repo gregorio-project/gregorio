6f69648f8a85906c3c18b1fcffda7944d15eb164
Br. Samuel Springuel <rpspringuel@gmail.com>
2015-03-15 13:35:46 -0400

    Version Number change and bug fix #82
    - The version number is changed to the new release number.  Note, for now I've left the API version number in place, but Élie and I are talking about reducing to a single version number.  If we decide to go that route we should update the version number again before marking this ready for official release.
    - I missed some distances when renaming them so that each type of distance was identified.  The resulted in some undefined control sequences.

--- tex/gregoriotex.lua
+++ tex/gregoriotex.lua
@@ -21,7 +21,7 @@ local hpack, traverse_id, has_attribute, count, remove, insert_after, copy = nod
 gregoriotex = gregoriotex or {}
 local gregoriotex = gregoriotex
 
-local internalversion = 20150311
+local internalversion = 20150315
 
 local err, warn, info, log = luatexbase.provides_module({
     name               = "gregoriotex",
