gregorio-project/gregorio

commit 455f15ffdc707a3e2f937e34c9a19d4de2b08601

Extended check to cover explicit output filenames.

src/gregorio-utils.c
@@ -151,6 +151,28 @@ available formats are:\n\
 \n"));
 }
 
+void
+check_input_clobber (char *input_file_name, char *output_file_name)
+{
+  char *real_input_file_name = realpath(input_file_name, NULL);
+  char *real_output_file_name = realpath(output_file_name, NULL);
+
+  if (real_input_file_name && real_output_file_name
+	  && strcmp(real_input_file_name, real_output_file_name) == 0)
+    {
+      fprintf (stderr, "error: refusing to overwrite the input file\n");
+      exit (-1);
+    }
+  if (real_input_file_name)
+	{
+	  free(real_input_file_name);
+	}
+  if (real_output_file_name)
+	{
+	  free(real_output_file_name);
+	}
+}
+
 int
 main (int argc, char **argv)
 {
@@ -162,8 +184,6 @@ main (int argc, char **argv)
   char *output_file_name = NULL;
   char *output_basename = NULL;
   char *error_file_name = NULL;
-  char *real_input_file_name = NULL;
-  char *real_output_file_name = NULL;
   FILE *input_file = NULL;
   FILE *output_file = NULL;
   FILE *error_file = NULL;
@@ -524,17 +544,7 @@ main (int argc, char **argv)
                 }
 #endif
             }
-          real_input_file_name = realpath(input_file_name, NULL);
-          real_output_file_name = realpath(output_file_name, NULL);
-          if (strcmp(real_input_file_name, real_output_file_name) == 0)
-            {
-              fprintf (stderr, "error: refusing to overwrite the input file\n");
-              exit (-1);
-            }
-          free(real_input_file_name);
-          real_input_file_name = NULL;
-          free(real_output_file_name);
-          real_output_file_name = NULL;
+		  check_input_clobber(input_file_name, output_file_name);
           output_file = fopen (output_file_name, "w");
           if (!output_file)
             {
@@ -548,6 +558,10 @@ main (int argc, char **argv)
     {
       if (!output_file)
         {
+          if (!input_file)
+            {
+              check_input_clobber(input_file_name, output_file_name);
+            }
           output_file = fopen (output_file_name, "w");
           if (!output_file)
             {

commit 51f5d43cb3b7791c61c1bdc15cad0b7f2db80838

Fixing mingw32 compilation
https://github.com/gregorio-project/gregorio/commit/455f15ffdc707a3e2f937e34c9a19d4de2b08601
broke windows cross-compilation, because mingw32 doesn't have a realpath
function. I used _fullpath instead, but I had to hardcode MAX_PATH to 260.
I don't really know how to improve that...

src/gregorio-utils.c
@@ -151,6 +151,12 @@ available formats are:\n\
 \n"));
 }
 
+
+// realpath is not in mingw32
+#ifdef __MINGW32__
+  #define realpath(path,resolved_path) _fullpath(resolved_path, path, 260)
+#endif
+
 void
 check_input_clobber (char *input_file_name, char *output_file_name)
 {