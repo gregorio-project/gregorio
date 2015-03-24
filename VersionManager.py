96ce32a687ca046f1ea9e6b7a7a6bf0bfd764510
Elie Roux <elie.roux@telecom-bretagne.eu>
2015-03-24 10:24:35 +0100

    adding --get-debian-main and --get-debian-git versions

--- VersionManager.py
+++ VersionManager.py
@@ -16,7 +16,8 @@ from __future__ import print_function
 import sys
 import re
 import argparse
-
+import subprocess
+import time
 
 version_file = '.gregorio-version'
 gregorio_files = ["configure.ac",
@@ -33,11 +34,29 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-c', '--get-current',
                     help='Prints the current gregorio version',
                     action='store_true')
+parser.add_argument('-d', '--get-debian-main',
+                    help='Prints the version converted for Debian package',
+                    action='store_true')
+parser.add_argument('-dg', '--get-debian-git',
+                    help='Prints the version converted for Debian git package',
+                    action='store_true')
 
 def fetch_version(versionfile):
     with open(versionfile, 'r') as vfile:
         grever = vfile.readline()
     return grever.strip('\n')
+
+def fetch_version_debian(versionfile):
+    with open(versionfile, 'r') as vfile:
+        grever = vfile.readline()
+    return grever.strip('\n').replace('-', '~')
+
+def fetch_version_debian_git(versionfile):
+    main_version = fetch_version_debian(versionfile)
+    short_tag = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip('\n')
+    #short_tag = git_process.strip('\n')
+    date = time.strftime("%Y%m%d")
+    return "%s+git%s+%s" % (main_version, date, short_tag)
 
 def fileinput(infile):
     "Reads a file into a list."
@@ -82,13 +101,23 @@ def current_version(version):
     print(version)
 
 def main():
-    GREGORIO_VERSION = fetch_version(version_file)
+    GREGORIO_VERSION = None
     args = parser.parse_args()
     if args.get_current:
+        GREGORIO_VERSION = fetch_version(version_file)
         current_version(GREGORIO_VERSION)
+        sys.exit(0)
+    elif args.get_debian_main:
+        GREGORIO_VERSION_DEBIAN = fetch_version_debian(version_file)
+        current_version(GREGORIO_VERSION_DEBIAN)
+        sys.exit(0)
+    elif args.get_debian_git:
+        GREGORIO_VERSION_DEBIAN_GIT = fetch_version_debian_git(version_file)
+        current_version(GREGORIO_VERSION_DEBIAN_GIT)
         sys.exit(0)
 
     global result
+    GREGORIO_VERSION = fetch_version(version_file)
     for f in gregoriotex_files:
         src = fileinput(f)
         replace_gregoriotex_version(src, GREGORIO_VERSION)
