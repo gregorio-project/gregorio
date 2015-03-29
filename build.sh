7d17c85c7503c0048eb8fdbc817f5cbe57f22852
Elie Roux <elie.roux@telecom-bretagne.eu>
2015-03-29 16:14:43 +0200

    Simplify compilation
     * simplify Makefiles: only one in src/
     * remove libtool dependency (less portability problems!)
     * simplify compilation

--- build.sh
+++ build.sh
@@ -35,7 +35,6 @@ CFLAGS="$CFLAGS -Wdeclaration-after-statement"
 until [ -z "$1" ]; do
   case "$1" in
     --mingw     ) MINGWCROSS=TRUE ;;
-    --static    ) STATIC=TRUE ;;
     --warn      ) WARN=TRUE ;;
     --host=*    ) CONFHOST="$1" ;;
     --build=*   ) CONFBUILD="$1" ;;
@@ -45,13 +44,6 @@ until [ -z "$1" ]; do
   esac
   shift
 done
-
-STATICFLAGS=
-
-if [ "$STATIC" = "TRUE" ]
-then
-  STATICFLAGS="--enable-static-ltdl --disable-shared --enable-all-static"
-fi
 
 B=build
 
@@ -83,7 +75,6 @@ then
   OLDPATH=$PATH
   PATH=/usr/$MINGWSTR/bin:$PATH
   CFLAGS="-mtune=pentiumpro -msse2 -O2 $CFLAGS"
-  STATICFLAGS="--enable-static-ltdl --disable-shared --enable-all-static"
   LDFLAGS="-Wl,--large-address-aware $CFLAGS"
   ARCHFLAGS="--target=\"$MINGWSTR\" \
     --with-gnu-ld \
@@ -91,7 +82,6 @@ then
     --build=$MINGWBUILD \
     --prefix=/usr/$MINGWSTR"
 else
-ARCHFLAGS="--enable-xml-read"
 if [ "$MACCROSS" = "TRUE" ]
 then
   # make sure that architecture parameter is valid
@@ -99,7 +89,7 @@ then
     i386 | x86_64 | ppc | ppc64 ) ;;
     * ) echo "ERROR: architecture $ARCH is not supported"; exit 1;;
   esac
-  ARCHFLAGS="$ARCHFLAGS --enable-static-ltdl"
+  ARCHFLAGS="$ARCHFLAGS"
   CFLAGS="-arch $ARCH -g -O2 $CFLAGS"
   LDFLAGS="-arch $ARCH $LDFLAGS" 
 fi  
@@ -120,7 +110,7 @@ then
   echo
 fi
 
-CONFIGURE_ARGS="$CONFHOST $CONFBUILD $STATICFLAGS $ARCHFLAGS $OTHERARGS"
+CONFIGURE_ARGS="$CONFHOST $CONFBUILD $ARCHFLAGS $OTHERARGS"
 echo "Configuring build files; options: $CONFIGURE_ARGS"
 ./configure $CONFIGURE_ARGS || die "configure Gregorio"
 echo
