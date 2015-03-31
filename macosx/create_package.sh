b88f4f682d8b2139550d354ecad934cb24f59a91
Br. Samuel Springuel <rpspringuel@gmail.com>
2015-03-31 16:49:09 -0400

    Adding Compiled Documentation
    Modifications to `create_package.sh` so that the `GregorioRef.pdf` is produced
    Modified `gregorio-doc.pkgproj` to install `GregorioRef-##.pdf` (note, does not account for the fact that the version number is in the file name and will change with each build).
    Modification to `.gitignore` so that the `_minited-…` folder will be ignored, regardless of what version of GregorioRef has been compiled.

--- macosx/create_package.sh
+++ macosx/create_package.sh
@@ -3,9 +3,11 @@
 HERE=`pwd`/build
 cd ..
 ./build.sh --arch=x86_64 --enable-all-static --disable-shared --enable-static-ltdl
-sudo make DESTDIR=$HERE install
+make dist
+make DESTDIR=$HERE install
 ./install-gtex.sh dir:$HERE
 pandoc ./README.md -f markdown -t html -o $HERE/README.html
 pandoc ./COPYING.md -f markdown -t html -o $HERE/COPYING.html
-
+cd $HERE
+sed -i.temp 
 
