4cda23319ecd88b38fce2ea688c5c73fb4c3f700
Br. Samuel Springuel <rpspringuel@gmail.com>
2015-03-31 16:31:39 -0400

    Continued work on Mac installer.
    Upgraded `gregorio-base` to be the Gregorio distribution package and started adding the presentation elements.
    Created a installer project to wrap the uninstall script.  This project has an empty payload and just runs the uninstall script as a post-flight script.
    Small edit to `create_package.sh` so that working directory is correct when running `build.sh` etc.

--- macosx/create_package.sh
+++ macosx/create_package.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-../build.sh --arch=x86_64 --enable-all-static --disable-shared --enable-static-ltdl
 HERE=`pwd`/build
+cd ..
+./build.sh --arch=x86_64 --enable-all-static --disable-shared --enable-static-ltdl
 sudo make DESTDIR=$HERE install
-../install-gtex.sh dir:$HERE
+./install-gtex.sh dir:$HERE
+pandoc ./README.md -f markdown -t html -o $HERE/README.html
+pandoc ./COPYING.md -f markdown -t html -o $HERE/COPYING.html
+
 
