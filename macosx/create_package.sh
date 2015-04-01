e985a86cd8062bc0bbca6a0b2e2656478e10ac1b
Br. Samuel Springuel <rpspringuel@gmail.com>
2015-04-01 10:00:18 -0400

    Adjustments to create_package.sh
    Changed name of directory variable to make it more descriptive.
    Eliminated post-sed rm.  The back-up files will be removed in the general clean-up at the end of the script.

--- macosx/create_package.sh
+++ macosx/create_package.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# NOTE: This script assumes that it was invoked from the macosx directory and changes the working directory as needed accordingly.  Failure to invoke it from that directory will result in strange things happening.
+
 # First we compile Gregorio and build all the necessary pacakge components.
-HERE=`pwd`/build
+BUILDDIR=`pwd`/build
 cd ..
 ./build.sh --arch=x86_64 --enable-all-static --disable-shared --enable-static-ltdl
-make DESTDIR=$HERE install
-./install-gtex.sh dir:$HERE
+make DESTDIR=$BUILDDIR install
+./install-gtex.sh dir:$BUILDDIR
 cd doc
 make doc
 cd ..
@@ -13,9 +15,8 @@ pandoc ./README.md -f markdown -t html -o $HERE/README.html
 pandoc ./COPYING.md -f markdown -t html -o $HERE/COPYING.html
 VERSION=`./VersionManager.py --get-current`
 HTMLPREFIX="https://github.com/gregorio-project/gregorio/blob/v"$VERSION"/"
-cd $HERE
+cd $BUILDDIR
 sed -i.temp -E "s|(href=.)([A-Z]+.md.)|\1$HTMLPREFIX\2|g" README.html COPYING.html
-rm *.temp
 
 
 # Now we build the packages
