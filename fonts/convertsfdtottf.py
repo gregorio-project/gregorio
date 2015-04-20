f790057969852eea3d3c7f7eb87cb3fc4df9dfe6
Br. Elijah Schwab, O.Carm <elijahschwab@gmail.com>
2015-04-20 10:34:18 -0500

    Update copyright statement in `convertsfdtottf.py`.

--- fonts/convertsfdtottf.py
+++ fonts/convertsfdtottf.py
@@ -2,29 +2,32 @@
 # coding=utf-8
 # pylint: disable=import-error
 
+
 """
     Python fontforge script to convert from fontforge's native sfd
     to a TrueType font (ttf).
 
-    Copyright (C) 2015 R. Padraic Springuel <rpspringuel@gmail.com>
+    Copyright (C) 2015 The Gregorio Project (see CONTRIBUTORS.md)
 
-    This program is free software: you can redistribute it and/or modify
+    This file is part of Gregorio.
+
+    Gregorio is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    Gregorio is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with Gregorio.  If not, see <http://www.gnu.org/licenses/>.
 
     This script takes a .sfd file and converts it to a .ttf file.
 
     Basic use :
-     ./convertsfdtottf.py fontname
+     fontforge -script convertsfdtottf.py fontname
 
 """
 
