9ea85fe9ea3eedad5ba5a03c2ef633227713d932
jperon <cataclop@hotmail.com>
2015-05-06 18:42:16 +0200

    Temporary file only
    This should work under Windows like that.

--- tex/gregoriotex.lua
+++ tex/gregoriotex.lua
@@ -281,24 +281,18 @@ local function include_score(input_file, force_gabccompile)
 end
 
 local function direct_gabc(gabc, header)
-  local gabccode = 'name:direct-gabc;\n'..(header or '')..'\n%%%%\n'..gabc
-  local f = io.popen('echo "'..gabccode..'" | gregorio -sS', 'r')
-  -- In shell-restricted mode, with gregorio (and even echo) allowed,
-  -- the previous command doesn't work ; in that case, use a temporary file.
-  if f == nil then
-    tmpname = os.tmpname()
-    local p = io.popen('gregorio -s -o '..tmpname, 'w')
-    if p == nil then
-      err("\nSomething went wrong when executing\n    gregorio -s -o "..tmpname..".\n"
-      .."shell-escape mode may not be activated. Try\n\n%s --shell-escape %s.tex\n\nSee the documentation of gregorio or your TeX\ndistribution to automatize it.", tex.formatname, tex.jobname)
-    end
-    p:write(gabccode)
-    p:close()
-    f = io.open(tmpname)
+  tmpname = os.tmpname()
+  local p = io.popen('gregorio -s -o '..tmpname, 'w')
+  if p == nil then
+    err("\nSomething went wrong when executing\n    gregorio -s -o "..tmpname..".\n"
+    .."shell-escape mode may not be activated. Try\n\n%s --shell-escape %s.tex\n\nSee the documentation of gregorio or your TeX\ndistribution to automatize it.", tex.formatname, tex.jobname)
   end
+  p:write('name:direct-gabc;\n'..(header or '')..'\n%%%%\n'..gabc)
+  p:close()
+  f = io.open(tmpname)
   tex.print(f:read("*a"):explode('\n'))
   f:close()
-  if tmpname then os.remove(tmpname) end
+  os.remove(tmpname)
 end
 
 local function check_font_version()
