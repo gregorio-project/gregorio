c91997cb58e81399339415fb461c51f78bf6005b
jperon <cataclop@hotmail.com>
2015-05-06 18:22:16 +0200

    Two dots forgotten…

--- tex/gregoriotex.lua
+++ tex/gregoriotex.lua
@@ -289,7 +289,7 @@ local function direct_gabc(gabc, header)
     tmpname = os.tmpname()
     local p = io.popen('gregorio -s -o '..tmpname, 'w')
     if p == nil then
-      err("\nSomething went wrong when executing\n    gregorio -s -o "..tmpname".\n"
+      err("\nSomething went wrong when executing\n    gregorio -s -o "..tmpname..".\n"
       .."shell-escape mode may not be activated. Try\n\n%s --shell-escape %s.tex\n\nSee the documentation of gregorio or your TeX\ndistribution to automatize it.", tex.formatname, tex.jobname)
     end
     p:write(gabccode)
