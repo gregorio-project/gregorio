b6eb93dc194beae00394a718bdcdf36ff7ecf3a3
jperon <cataclop@hotmail.com>
2015-05-06 17:34:53 +0200

    Error message when no shell-access at all

--- tex/gregoriotex.lua
+++ tex/gregoriotex.lua
@@ -288,6 +288,10 @@ local function direct_gabc(gabc, header)
   if f == nil then
     tmpname = os.tmpname()
     local p = io.popen('gregorio -s -o '..tmpname, 'w')
+    if p == nil then
+      err("\nSomething went wrong when executing\n    'gregorio -sS'.\n"
+      .."shell-escape mode may not be activated. Try\n\n%s --shell-escape %s.tex\n\nSee the documentation of gregorio or your TeX\ndistribution to automatize it.", tex.formatname, tex.jobname)
+    end
     p:write('name:tmp;\n%%%%\n'..entree)
     p:close()
     f = io.open(tmpname)
