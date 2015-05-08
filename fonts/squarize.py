fb5f91afb16f1824803b6d137e91ca691dc1fb48
Elie Roux <elie.roux@telecom-bretagne.eu>
2015-05-08 20:38:46 +0200

    add episemus for virga with bar

--- fonts/squarize.py
+++ fonts/squarize.py
@@ -516,6 +516,7 @@ def hepisemus(widths):
     write_hepisemus(widths, get_width(widths, 'QuilismaLineTR'), 'HEpisemusQuilismaLineTR')
     write_hepisemus(widths, get_width(widths, 'PunctumSmall'), 'HEpisemusHighPes')
     write_hepisemus(widths, get_width(widths, 'Oriscus'), 'HEpisemusOriscus')
+    write_hepisemus(widths, get_width(widths, 'VirgaLineBR'), 'HEpisemusVirgaLineBR')
     write_hepisemus(widths, get_width(widths, 'OriscusLineTR'), 'HEpisemusOriscusLineTR')
     write_hepisemus(widths, get_width(widths, 'PunctumLineBR'), 'HEpisemusPunctumLineBR')
     write_hepisemus(widths, get_width(widths, 'PunctumLineBL'), 'HEpisemusPunctumLineBL')
