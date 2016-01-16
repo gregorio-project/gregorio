94a46696343915f8316325d4d4da9faff11ec3ca
Br. Samuel Springuel <rpspringuel@gmail.com>
2016-01-16 13:25:38 -0500

    Revising TeXShop configuration script
    This adds `gabc` to the list of file formats which TeXShop can save when saving a new file (previously, you had to save it as `.tex` and then change the extension in Finder)
    I've also added `gaux` and `gtex` to the list of files removed by the "Trash Aux Files" command.  Note that since the `gtex` files generally do not have the same basename as the project file, they will only be deleted when "Trash Aux Files" is in aggressive mode.

--- contrib/TeXShop/auto-configure.sh
+++ contrib/TeXShop/auto-configure.sh
@@ -2,6 +2,7 @@
 
 # This script is designed to automatically configure a TeXShop distribution.
 
+#Copy the engine file from its instalation directory to the TeXShop Engines directory
 ENGINEDIR="$HOME/Library/TeXShop/Engines"
 if [ ! -d "$ENGINEDIR" ]; then
     mkdir -p "$ENGINEDIR"
@@ -14,4 +15,38 @@ else
     echo "Please try running the Gregorio intaller again"
     exit 1
 fi
+
+#Add 'gabc' to the list of file extensions which TeXShop knows
+TeXShopDir=`osascript -e 'POSIX path of (path to app "TeXShop")'`
+
+defaults write "$TeXShopDir/Contents/Info.plist" CFBundleDocumentTypes -array-add '<dict>
+<key>CFBundleTypeExtensions</key>
+<array>
+<string>gabc</string>
+</array>
+<key>CFBundleTypeName</key>
+<string>gabc</string>
+<key>CFBundleTypeOSTypes</key>
+<array>
+<string>GABC</string>
+</array>
+<key>CFBundleTypeRole</key>
+<string>Editor</string>
+<key>LSItemContentTypes</key>
+<array>
+<string>com.unknown.gabc</string>
+</array>
+<key>LSTypeIsPackage</key>
+<false/>
+<key>NSDocumentClass</key>
+<string>TSDocument</string>
+<key>NSPersistentStoreTypeKey</key>
+<string>Binary</string>
+</dict>'
+
+#enable syntax coloring and the Typeset button for gabc files
 defaults write TeXShop OtherTeXExtensions -array-add "gabc"
+
+#Add gtex and gaux to the list of aux files deleted with Trash Aux Files
+defaults write TeXShop OtherTrashExtensions -array-add "gtex"
+defaults write TeXShop OtherTrashExtensions -array-add "gaux"
